57d5cb47dee833e2418c96e3d2a99251d4b9e8c6
Igors Gulbinskis <igors.gulbinskis@ericsson.com>
2013-08-18 00:37:03 +0300

    FB friends rendered in columns

--- WebContent/local/js/game-save/friend.js
+++ WebContent/local/js/game-save/friend.js
@@ -3,14 +3,29 @@
  */
 var friendController = function() {
 	
+	var COLUMNS = 3;
+	
 	function _renderFriends() {
 		facebookController.getFriends(function(friends) {
-			var html = '';
+			var html = '<table style="width: 100%;">';
 			if (friends != null) {
 				for (var i = 0; i < friends.length; i++) {
-					html += '<input type="checkbox" />&nbsp;' + friends[i].name + '<br />';
+					html += '<tr>';
+					for (var j = 0; j < COLUMNS; j++) {
+						html += '<td>';
+							html += '<input type="checkbox" value="' + friends[i].id + '" />&nbsp;' + friends[i].name + '<br />';
+						html += '</td>';
+						
+						if (i >= friends.length - 1) {
+							break;
+						} else {
+							i++;
+						}
+					}
+					html += '</tr>';
 				}
 			}
+			html += '</table>';
 			$('#friends').html(html);
 		});
 	}
